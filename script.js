1c39bc6fb075d988ea4e1a5b291f927e78ad2e90
ddaiivan <daivanfebrijuansetiya@gmail.com>
2025-03-15 11:30:47 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -14,34 +14,13 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 
-    // --- Konsultasi Dokter Modal Handling ---
-    // Get the modal
-    var modal = document.getElementById("konsultasiModal");
+    consultationButton.addEventListener('click', function() {
+        document.getElementById('halodocModal').style.display = 'block';
+    });
 
-    // Get the button that opens the modal
-    var btn = document.getElementById("consultationButton");
-
-    // Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close")[0];
-
-    // When the user clicks the button, open the modal
-    btn.onclick = function() {
-      modal.style.display = "block";
-    }
-
-    // When the user clicks on <span> (x), close the modal
-    span.onclick = function() {
-      modal.style.display = "none";
-    }
-
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function(event) {
-      if (event.target == modal) {
-        modal.style.display = "none";
-      }
-    }
-    // --- End Konsultasi Dokter Modal Handling ---
-
+    document.getElementById('closeModal').addEventListener('click', function() {
+        document.getElementById('halodocModal').style.display = 'none';
+    });
 
     document.getElementById('communityButton').addEventListener('click', function() {
         document.getElementById('communityModal').style.display = 'block';
